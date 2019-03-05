449ca541620fe9fb63aef154cdc2178b838b190d
aravind <aravind.k@tvsnext.io>
2019-03-05 02:02:44 -0500

    updated design

--- src/main/java/com/payconiq/controller/DataLoader.java
+++ src/main/java/com/payconiq/controller/DataLoader.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 
 @Component
 public class DataLoader {
@@ -23,6 +25,19 @@ public class DataLoader {
         Stock stock2=new Stock("platinum",Amount.Of(2500.0D),1551548467868l);
         Stock stock3=new Stock("sliver",Amount.Of(3400.0D), Instant.now().toEpochMilli());
         Stock stock4=new Stock("bitcoin",Amount.Of(1500.0D),Instant.now().toEpochMilli());
+        java.util.List<String> datas= Arrays.asList("gold","platinum","sliver","bitcoin");
+        for(String data:datas){
+            for(int i=0;i<10;i++){
+                double amount=0;
+                if(i%2==0){
+                    amount=2500.0D;
+                }else{
+                    amount=750.0D;
+                }
+                long ts=Instant.now().minus(5+i, ChronoUnit.HOURS).toEpochMilli();
+                stockStorageRepo.addStock(new Stock(data,Amount.Of(amount),ts));
+            }
+        }
         stockStorageRepo.addStock(stock1);
         stockStorageRepo.addStock(stock2);
         stockStorageRepo.addStock(stock3);
--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -6,9 +6,15 @@ $(document).ready(function () {
                   fire_ajax_submit();
      });
 
+    $('#stocks').change(function(){
+                 var optionSelected = $("option:selected", this);
+                     var valueSelected = this.value;
+                     console.log(valueSelected)
+                     loadStocksGraph(valueSelected);
+            });
      fire_ajax_submit();
      loadStocks();
-
+    $(".stockForm").toggle();
      $("#addStock").click(function(){
            var sn= $("#stockName").val();
            var amt= $("#amount").val();
@@ -75,46 +81,25 @@ function fire_ajax_submit() {
         dataType: 'json',
         cache: false,
         timeout: 600000,
-        success: function (data) {
+        success: function(data) {
+        var dataSet=[]
             $.each(data,function(i,item){
-                 var date = new Date(item.lastUpdate);
-                 var $tr = $('<tr class="stockRow">').append(
-                            $('<td>').text(item.id),
-                            $('<td>').text(item.name),
-                            $('<td>').text(item.currentPrice.amount),
-                            $('<td>').text(item.currentPrice.unit),
-                            $('<td>').text(item.currentPrice.currency),
-
-                            $('<td>').text(date.toString("MM-DD-YYYY HH:MM")),
-                        );
-                    $('#stockrecords').append($tr);
+                var dateTime = new Date(item.lastUpdate);
+                dateTime = moment(dateTime).format("YYYY-MM-DD HH:mm:ss");
+                dataSet.push([item.id,item.name,item.currentPrice.formatted,dateTime]);
             });
+            console.log(dataSet)
+             $('#stockrecords').DataTable( {
+                    data: dataSet,
+                    columns: [
+                        { title: "#" },
+                        { title: "Name" },
+                        { title: "Price" },
+                        { title: "LastUpdated"}
+                    ]
+                } );
 
             console.log("SUCCESS : ", data);
-              $('#stockrecords').each(function() {
-                      var currentPage = 0;
-                      var numPerPage = 2;
-                      var $table = $(this);
-                      $table.bind('repaginate', function() {
-                          $table.find('tbody tr').hide().slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
-                      });
-                      $table.trigger('repaginate');
-                      var numRows = $table.find('tbody tr').length;
-                      var numPages = Math.ceil(numRows / numPerPage);
-                      console.log(numPages);
-                      var $pager = $('<div class="pager"></div>');
-                      for (var page = 0; page < numPages; page++) {
-                          $('<span class="page-number"></span>').text(page + 1).bind('click', {
-                              newPage: page
-                          }, function(event) {
-                              currentPage = event.data['newPage'];
-                              $table.trigger('repaginate');
-                              $(this).addClass('active').siblings().removeClass('active');
-                          }).appendTo($pager).addClass('clickable');
-                      }
-                      $pager.insertBefore($table).find('span.page-number:first').addClass('active');
-                  });
-
         },
         error: function (e) {
 
@@ -142,30 +127,49 @@ function loadStocks(){
         success: function (data) {
             $.each(data,function(i,item){
                  var date = new Date(item.lastUpdate);
-                 $('#stocks').append($("<option>").val(item).text(item););
+                 $('#stocks').append($("<option>").val(item).text(item));
             });
 
             console.log("SUCCESS : ", data);
-            var firstData=$("#stocks").val($("#stocks option:first").val());
-            time=['x'];
+            var firstData=$("#stocks option:first-child").val();
+            console.log(firstData);
+                loadStocksGraph(firstData);
+        },
+        error: function (e) {
+
+            var json = "<h4>Ajax Response</h4><pre>"
+                + e.responseText + "</pre>";
+            $('#feedback').html(json);
+
+            console.log("ERROR : ", e);
+            $("#btn-search").prop("disabled", false);
+
+        }
+    });
+
+}
+
+
+function loadStocksGraph(firstData){
+  time=['x'];
             amount=['amount'];
 
              $.ajax({
                     type: "GET",
                     contentType: "application/json",
-                    url: "/api/stocks/"+firstData,
+                    url: "/api/stock/"+firstData,
                     dataType: 'json',
                     cache: false,
                     timeout: 600000,
                     success: function (data) {
                         $.each(data,function(i,item){
-                           time.append(item.lastUpdate);
-                           amount.append(item.currentPrice.amount);
+                           time.push(item.lastUpdate);
+                           amount.push(item.currentPrice.amount);
                         });
 
                         console.log("SUCCESS : ", data);
                         c3.generate({
-                            bindTo:'#charts',
+                            bindto:'#charts',
                             data: {
                                 x: 'x',
                                 columns: [
@@ -177,7 +181,7 @@ function loadStocks(){
                                 x: {
                                     type: 'timeseries',
                                     tick: {
-                                        format: '%Y-%m-%d hh:mm'
+                                        format: '%Y-%m-%d %H:%M:%S'
                                     }
                                 }
                             }
@@ -196,18 +200,5 @@ function loadStocks(){
 
                     }
                 });
-
-        },
-        error: function (e) {
-
-            var json = "<h4>Ajax Response</h4><pre>"
-                + e.responseText + "</pre>";
-            $('#feedback').html(json);
-
-            console.log("ERROR : ", e);
-            $("#btn-search").prop("disabled", false);
-
-        }
-    });
 
 }
